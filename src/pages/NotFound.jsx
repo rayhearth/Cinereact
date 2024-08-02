030919eb09f7878873023913fdef06bc47fe4502
User <emmanuellejupon@gmail.com>
2024-08-02 16:24:10 +0200

    catch the data for application

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,17 +1,14 @@
-import React from 'react';
-import {NavLink} from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
-  return (
-    <div className='notFound'>
-      <div className="notFound-content">
-        <h2>Erreur 404</h2>
-        <NavLink to="/">
-        <h3>Retour à l'accueil <i className="fas fa-home"></i></h3>
-        </NavLink>
-      </div>
-    </div>
-  );
+	return (
+		<div className="pageError-404">
+			<h1>404</h1>
+			<p>La page que vous demandez n'existe pas.</p>
+			<Link to="/">Retour à l'accueil<i className="fas fa-home"></i></Link>
+		</div>
+	);
 };
 
 export default NotFound;
