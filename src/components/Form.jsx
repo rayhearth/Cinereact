83be6a4196d2a24a7e388684d72a620eeaa30ce2
User <emmanuellejupon@gmail.com>
2024-08-02 18:33:43 +0200

    work on form component

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -12,8 +12,16 @@ const Form = () => {
 					/>
 					<input type="submit" value="Recherchez" />
 				</form>
-        <div className="btn-sort-container"></div>
+        <div className="btn-sort-container">
+					<div className="btn-sort" id="goodToBad">
+						Top <i class="fa-solid fa-arrow-up"></i>
+					</div>
+					<div className="btn-sort" id="badToGood">
+						Flop <i class="fa-solid fa-arrow-down"></i>
+					</div>
+				</div>
 			</div>
+			<div className="result">Résultat</div>
 		</div>
 	);
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,29 @@
 import React from 'react';
+import { useEffect, useState } from "react";
+import axios from "axios";
 import Header from '../components/Header';
 import Form from '../components/Form';
 
 const Home = () => {
+
+  const [data, setData] = useState([]);
+
+	useEffect(() => {
+		axios
+			.get(
+				"https://api.themoviedb.org/3/search/movie?api_key=ed82f4c18f2964e75117c2dc65e2161d&query=code&language=fr-FR"
+			)
+			.then((res) => {
+				if (res.data && res.data.results) {
+					setData(res.data.results);
+					console.log(res);
+				}
+			})
+			.catch((error) => {
+				console.error("Error fetching data:", error);
+			});
+	}, []);
+
   return (
     <div className='home-page'>
       <Header/>
