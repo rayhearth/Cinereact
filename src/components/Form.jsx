5e822a15831d2b8748b86c89db11b4ef18e9432e
User <emmanuellejupon@gmail.com>
2024-08-02 18:49:31 +0200

    add data in form component

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Form = () => {
+	const [moviesData, setMoviesData] = useState([]);
+
+	useEffect(() => {
+		axios
+			.get(
+				`https://api.themoviedb.org/3/search/movie?api_key=ed82f4c18f2964e75117c2dc65e2161d&query=code&language=fr-FR`
+			)
+			.then((res) => setMoviesData(res.data.results));
+	}, []);
+
 	return (
 		<div className="form-component">
 			<div className="form-container">
@@ -12,16 +23,20 @@ const Form = () => {
 					/>
 					<input type="submit" value="Recherchez" />
 				</form>
-        <div className="btn-sort-container">
+				<div className="btn-sort-container">
 					<div className="btn-sort" id="goodToBad">
-						Top <i class="fa-solid fa-arrow-up"></i>
+						Top <i className="fa-solid fa-arrow-up"></i>
 					</div>
 					<div className="btn-sort" id="badToGood">
-						Flop <i class="fa-solid fa-arrow-down"></i>
+						Flop <i className="fa-solid fa-arrow-down"></i>
 					</div>
 				</div>
 			</div>
-			<div className="result">Résultat</div>
+			<div className="result">
+				{moviesData.slice(1, 12).map((movie) => (
+					<h3>{movie.title}</h3>
+				))}
+			</div>
 		</div>
 	);
 };
