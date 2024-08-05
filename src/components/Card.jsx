303e94523205d79d55d8aa2f300312a81a09324f
User <emmanuellejupon@gmail.com>
2024-08-05 18:10:42 +0200

    create dateformateur

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,16 +1,27 @@
 import React from "react";
 
 const Card = ({ movie }) => {
+
+  const dateFormateur = (date) => {
+    let [yy, mm, dd] = date.split("-");
+    return [dd, mm,yy].join("/")
+  }
+
+
 	return (
 		<div className="card">
 			<img
 				src={
 					movie.poster_path
-						? "https://image.tmdb.org/t/p/original" + movie.poster_path
+						? "https://image.tmdb.org/t/p//original" + movie.poster_path
 						: "./poster.jpg"
 				}
-				alt={`affiche" + ${movie.title}`}
+				alt={`affiche de" + ${movie.title}`}
 			/>
+      <h2>{movie.title}</h2>
+      {movie.release_date ? 
+        <h5>Sorti le : {dateFormateur(movie.release_date)}</h5> : null
+  }
 		</div>
 	);
 };
