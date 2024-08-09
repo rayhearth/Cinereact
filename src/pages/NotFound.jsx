0aea3b83c45282be5561002a6716b6fcadd952f7
User <emmanuellejupon@gmail.com>
2024-08-09 14:19:27 +0200

    add media queries in img 404 to responsive

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -4,10 +4,12 @@ import { Link } from "react-router-dom";
 const NotFound = () => {
 	return (
 		<div className="pageError-404">
-			<img src="./img-404.png" alt="" />
-			<h1>404</h1>
+			<img src="./img-404.png" alt="image 40 de storyset sur freepik" />
+
 			<p>La page que vous demandez n'existe pas.</p>
-			<Link className="btn-return" to="/"><i className="fas fa-home"></i> Retour à l'accueil </Link>
+			<Link className="btn-return" to="/">
+				<i className="fas fa-home"></i> Retour à l'accueil{" "}
+			</Link>
 		</div>
 	);
 };
