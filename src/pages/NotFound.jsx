2f2eb8edcfe97de398555f69e61135fd88947cca
User <emmanuellejupon@gmail.com>
2024-08-09 14:06:22 +0200

    add css in page 404 and path the gradiant background

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -4,9 +4,10 @@ import { Link } from "react-router-dom";
 const NotFound = () => {
 	return (
 		<div className="pageError-404">
+			<img src="./img-404.png" alt="" />
 			<h1>404</h1>
 			<p>La page que vous demandez n'existe pas.</p>
-			<Link to="/">Retour à l'accueil<i className="fas fa-home"></i></Link>
+			<Link className="btn-return" to="/"><i className="fas fa-home"></i> Retour à l'accueil </Link>
 		</div>
 	);
 };
